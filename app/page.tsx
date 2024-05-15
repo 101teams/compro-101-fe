75215c56cac080733ec2d3ead8941e577dc96848
ichaspg <ichaspurnama12@gmail.com>
2024-05-15 09:45:07 +0700

    fix swr cors

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,11 @@ const fetcher = async (url: string) => {
   const options = {
     headers: {
       Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`,
+      "Access-Control-Allow-Credentials": "true",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+      "Access-Control-Allow-Headers":
+        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
     },
   };
   const request = await fetch(url, options);
