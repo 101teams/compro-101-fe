81313fab0c80134b71ba0b0bfe49317a1a87f76f
ichaspg <ichaspurnama12@gmail.com>
2024-05-15 09:01:20 +0700

    fixx cors

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/api/works?populate=*",
+        source: "/api/works/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
@@ -31,7 +31,7 @@ const nextConfig = {
         ],
       },
       {
-        source: "/api/services?populate=*",
+        source: "/api/services/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
