c1ff1f103e9cfdea4986e6872dc84868423c3f5e
Sigit Nurmajid <sigitnurmajid32@gmail.com>
2024-06-27 23:13:56 +0700

    fix shacky

--- components/ServiceCategory.tsx
+++ components/ServiceCategory.tsx
@@ -13,8 +13,9 @@ const ServiceCategory = ({
     <button
       className={`service__category_btn ${isActive ? "active" : ""}`}
       style={{
-        border: isActive ? "1px solid #9b9b9b" : "none",
+        border: isActive ? "1px solid #9b9b9b" : "1px solid transparent",
         outline: "none",
+      
       }}
       onClick={() => onClick(category)}
     >
