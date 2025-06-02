2ecfd0f9fcee181a4705f3b832a9c5f115fb9702
ichaspg <ichaspurnama12@gmail.com>
2025-06-02 13:28:13 +0700

    fix : change title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "101 Compro",
+  title: "101 Dev Team",
   description: "Your partners in crafting unforgettable digital experiences",
 };
 
